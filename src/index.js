d57d3f7578477118e9ed8c0aa2a35baf52a84846
CaioXM01 <caioxm01@gmail.com>
2019-11-19 02:38:07 -0300

    React Navigation and Styles

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  conteiner: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-console.tron.log('Hello Caio');
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View style={styles.conteiner}>
-      <Text style={styles.welcome}>Hello World!</Text>
-      <Text style={styles.welcome}>Caio Xavier.</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
   );
 }
